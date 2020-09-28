a9cc683f4d9e885a809680c53833c0ff3d91ccdc
lanyuxqh <873101399@qq.com>
2020-09-28 20:39:26 +0800

    '初始化项目'

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '懒鱼博客权限管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
